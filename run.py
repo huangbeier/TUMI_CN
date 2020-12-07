46b62bb9475e9f843b1844c0dcdd14af6cc4ee99
huangbeier <953564459@qq.com>
2020-12-07 23:53:00 +0800

    添加注释

--- run.py
+++ run.py
@@ -8,8 +8,8 @@ testcase = unittest.defaultTestLoader.discover('case','test_*.py')
 
 #自动运行case并生成报告 htmltestrunner
 filePath = r'F:\Tuogo_Project\TUMI\report\report.html'
-#F:\Tuogo_Project\TUMI\report\report.html
-#C:\huang111\TUMI_CN\report\report.html
+#F:\Tuogo_Project\TUMI\report\report.html  家里
+#C:\huang111\TUMI_CN\report\report.html   公司
 title = 'WEB自动化测试报告'
 descr = '这是自动化测试描述'
 
