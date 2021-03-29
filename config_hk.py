d0c1e956a4130a9d12cd92abad7e7c52605985d2
huangbeier <953564459@qq.com>
2021-03-29 11:33:31 +0800

    删除无用文件

--- config_hk.py
+++
@@ -1,6 +0,0 @@
-# @Author ：黄贝尔
-# @Time ：2020/12/4__15:01
-host='https://www.tumi-hk.com'
-kefu_url='https://v1.live800.com/live800/chatClient/chatbox.jsp?companyID=1103215&configID=46295&jid=1780232594&s=1&lan=en'
-chromeDriver_Path=r'C:\Users\huang\Desktop\chromedriver.exe'
-#'F:\Tuogo_Project\TUMI-HK\chromedriver.exe
